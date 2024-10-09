2b5565f40864e75196289fe4b2099aad90a1c5fa
Dima <dima@dima.domin>
2024-10-09 16:12:07 +0300

    Added func

--- genqr/genqr.go
+++ genqr/genqr.go
@@ -5,12 +5,12 @@ import (
 	"github.com/skip2/go-qrcode"
 	"image"
 	"image/draw"
-	"image/png"
+	"image/jpeg"
 	"os"
 )
 
 func GenerateQrCode(data string, logo *os.File) (*image.RGBA, error) {
-	qr, err := qrcode.New(data, qrcode.Medium)
+	qr, err := qrcode.New(data, qrcode.Highest)
 	if err != nil {
 		return nil, err
 	}
@@ -18,19 +18,28 @@ func GenerateQrCode(data string, logo *os.File) (*image.RGBA, error) {
 	return addLogoInQr(qr.Image(600), logo)
 }
 func addLogoInQr(qrCodeImage image.Image, logoFile *os.File) (*image.RGBA, error) {
+	logoWidth := 250
+	logoHeight := 95
 
-	logo, err := png.Decode(logoFile)
+	// Декодирование логотипа
+	logo, err := jpeg.Decode(logoFile)
 	if err != nil {
 		return nil, err
 	}
 
-	logoSize := 64
-	resizedLogo := resize.Resize(uint(logoSize), uint(logoSize), logo, resize.Lanczos3)
+	// Масштабирование логотипа до заданных размеров
+	resizedLogo := resize.Resize(uint(logoWidth), uint(logoHeight), logo, resize.Lanczos3)
 
-	offset := image.Pt((qrCodeImage.Bounds().Dx()-logoSize)/2, (qrCodeImage.Bounds().Dy()-logoSize)/2)
+	// Определение смещения для центровки логотипа
+	offset := image.Pt((qrCodeImage.Bounds().Dx()-logoWidth)/2, (qrCodeImage.Bounds().Dy()-logoHeight)/2)
+
+	// Создание нового изображения с учетом размеров QR-кода
 	b := qrCodeImage.Bounds()
 	m := image.NewRGBA(b)
+
+	// Рисование QR-кода и логотипа на новом изображении
 	draw.Draw(m, b, qrCodeImage, image.Point{}, draw.Src)
 	draw.Draw(m, resizedLogo.Bounds().Add(offset), resizedLogo, image.Point{}, draw.Over)
+
 	return m, nil
 }
--- main.go
+++ main.go
@@ -17,13 +17,20 @@ import (
 var (
 	configFileName = flag.String("config", "local", "Имя конфигурационного файла (без расширения .ini)")
 	verbose        = flag.Bool("verbose", false, "Включить режим детального вывода")
+	savePath       = flag.String("savePath", "utils/qr-code", "Имя конфигурационного файла (без расширения .ini)")
 )
 
 type contactData struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Phone     string `json:"phone"`
-	Email     string `json:"email"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Phone       string `json:"phone"`
+	Email       string `json:"email"`
+	CompanyName string `json:"company_name"`
+	UrlSite     string `json:"url_site"`
+	Street      string `json:"street"`
+	City        string `json:"city"`
+	Postcode    string `json:"postcode"`
+	Country     string `json:"country"`
 }
 
 type configData struct {
@@ -80,10 +87,27 @@ func qrCodesHandler(logo string) http.HandlerFunc {
 			w.Write([]byte(err.Error()))
 			return
 		}
-		vCard := fmt.Sprintf("BEGIN:VCARD\nVERSION:3.0\nFN:%s %s\nTEL:%s\nEMAIL:%s\nEND:VCARD",
-			contact.FirstName, contact.LastName, contact.Phone, contact.Email)
+		vCard := fmt.Sprintf(
+			"BEGIN:VCARD\n"+
+				"VERSION:3.0\n"+
+				"FN:%s %s\n"+
+				"N:%s;%s;;;\n"+
+				"TEL:%s\n"+
+				"EMAIL:%s\n"+
+				"ORG:%s\n"+
+				"URL:%s\n"+
+				"ADR;TYPE=WORK:;;%s;%s;;%s;%s\n"+
+				"END:VCARD",
+			contact.FirstName, contact.LastName,
+			contact.LastName, contact.FirstName,
+			contact.Phone,
+			contact.Email,
+			contact.CompanyName,
+			contact.UrlSite,
+			contact.Street, contact.City,
+			contact.Postcode, contact.Country)
 
-		logoFile, err := os.Open(logo + "Logo.svg")
+		logoFile, err := os.Open(logo + "Logo.png")
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte("Failed to open logo file: " + err.Error()))
@@ -98,6 +122,22 @@ func qrCodesHandler(logo string) http.HandlerFunc {
 		}
 		w.Header().Set("Content-Type", "image/png")
 
+		outputFileName := fmt.Sprintf("%s/%s_%s_qrcode.png", *savePath, contact.FirstName, contact.LastName)
+
+		outputFile, err := os.Create(outputFileName)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Failed to create output file: " + err.Error()))
+			return
+		}
+		defer outputFile.Close()
+
+		err = png.Encode(outputFile, pngQr)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Failed to save PNG to file: " + err.Error()))
+			return
+		}
 		err = png.Encode(w, pngQr)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
